332c014a718a881bcf762a7fdd0fd2fce7390333
RedaFarissi <redaesskouni@gmail.com>
2023-04-04 23:21:34 +0000

    'change'

--- project/settings.py
+++ project/settings.py
@@ -45,11 +45,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = ( 
     'http://localhost:8000', 
     'http://localhost:3000', 
 )
+
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly', #new
